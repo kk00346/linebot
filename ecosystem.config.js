f2950f5fef2679a71e435fe21869397432205d9b
Steven <steven@Stevende-MacBook-Air.local>
2021-10-28 20:56:43 +0800

    upa

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
       "user": "root",
       "host": ["139.162.112.61"],
       "ref": "origin/master",
-      "repo": "git@bitbucket.org:mejor-technology/node-linebot.git",
+      "repo": "git@github.com:kk00346/linebot.git",
       "path": "/root/node/linebot",
       "pre-deploy": "git fetch --all",
       "post-deploy": "npm install && pm2 startOrRestart ecosystem.config.js --env production"
